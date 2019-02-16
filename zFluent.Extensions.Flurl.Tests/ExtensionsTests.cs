ee4a5622562ead7597bacfa26e3aa4bba0216f1d
AALOON <zabirov.albert@gmail.com>
2019-02-16 17:02:07 +0300

    added more tests and new xml comments

--- zFluent.Extensions.Flurl.Tests/ExtensionsTests.cs
+++ zFluent.Extensions.Flurl.Tests/ExtensionsTests.cs
@@ -66,9 +66,55 @@ namespace zFluent.Extensions.Flurl.Tests
             _httpTest.CallLog.Count.Should().Be(3);
         }
 
+        [Fact]
+        public async Task SimpleActionTest_SucceedAsync()
+        {
+            // assign
+            _httpTest.SimulateTimeout();
+            _httpTest.RespondWith(JsonConvert.SerializeObject("Bad"), 503);
+            _httpTest.RespondWith(JsonConvert.SerializeObject("Ok"), 200);
+
+            var flurlClient = new FlurlClient("http://example120.com/");
+
+            await flurlClient.Request("api", "controller")
+                .AllowHttpStatus(HttpStatusCode.OK)
+                .WithRetryFuncAsync(RetryActionAsync)
+                .RetryAsync(p => p
+                    .PostUrlEncodedAsync(JsonConvert.SerializeObject("data")));
+            
+            _httpTest.CallLog.Count.Should().Be(3);
+        }
+
+        [Fact]
+        public async Task SimpleActionTest_ThrowsExceptionAsync()
+        {
+            // assign
+            _httpTest.SimulateTimeout();
+            _httpTest.RespondWith(JsonConvert.SerializeObject("Bad"), 503);
+            _httpTest.RespondWith(JsonConvert.SerializeObject("Bad"), 503);
+
+            var flurlClient = new FlurlClient("http://example120.com/");
+
+            Func<Task> act = () =>
+                flurlClient.Request("api", "controller")
+                    .AllowHttpStatus(HttpStatusCode.OK)
+                    .WithRetryFuncAsync(RetryActionAsync)
+                    .RetryAsync(p => p
+                        .PostUrlEncodedAsync(JsonConvert.SerializeObject("data"))); 
+
+            await act.Should().ThrowAsync<FlurlHttpException>();
+
+            _httpTest.CallLog.Count.Should().Be(3);
+        }
+
         private async Task<string> RetryFuncAsync(IFlurlRequest request, Func<IFlurlRequest, Task<string>> func)
         {
             return await _policy.ExecuteAsync(() => func(request));
+        }
+
+        private async Task RetryActionAsync(IFlurlRequest request, Func<IFlurlRequest, Task> func)
+        {
+            await _policy.ExecuteAsync(() => func(request));
         }
 
         public void Dispose()
--- zFluent.Extensions.Flurl/FluentExtensions.cs
+++ zFluent.Extensions.Flurl/FluentExtensions.cs
@@ -6,6 +6,13 @@ namespace zFluent.Extensions.Flurl
 {
     public static class FluentExtensions
     {
+        /// <summary>
+        /// Executes retry function with argument type IFlurlRequest
+        /// </summary>
+        /// <typeparam name="TResult">Type of Argument of extension</typeparam>
+        /// <param name="context">Context which proceeded by WithRetryFunc</param>
+        /// <param name="func">The function which will be retried</param>
+        /// <returns>Result of the function or throw some exception</returns>
         public static TResult Retry<TResult>(this IFluentContext<IFlurlRequest, TResult> context,
             Func<IFlurlRequest, TResult> func)
         {
@@ -14,12 +21,27 @@ namespace zFluent.Extensions.Flurl
             return context.RetryFunc(context.Argument, func);
         }
 
+        /// <summary>
+        /// Setups retry function
+        /// </summary>
+        /// <typeparam name="TResult">Type of Result of the function</typeparam>
+        /// <param name="argument">Base argument from which extension started</param>
+        /// <param name="func">Retry function</param>
+        /// <returns>Retry retry context</returns>
+        /// <exception cref="ArgumentNullException">Func cannot be null</exception>
         public static IFluentContext<IFlurlRequest, TResult> WithRetryFunc<TResult>(this IFlurlRequest argument,
             Func<IFlurlRequest, Func<IFlurlRequest, TResult>, TResult> func)
         {
             return argument.WithRetryFunc<IFlurlRequest, TResult>(func);
         }
 
+        /// <summary>
+        /// Executes async retry function with argument type IFlurlRequest
+        /// </summary>
+        /// <typeparam name="TResult">Type of Result of the function</typeparam>
+        /// <param name="context">Context which proceeded by WithRetryFuncAsync</param>
+        /// <param name="func">The function which will be retried</param>
+        /// <returns>Result of the function or throw some exception</returns>
         public static Task<TResult> RetryAsync<TResult>(this IFluentContextAsync<IFlurlRequest, TResult> context,
             Func<IFlurlRequest, Task<TResult>> func)
         {
@@ -28,6 +50,13 @@ namespace zFluent.Extensions.Flurl
             return context.RetryFunc(context.Argument, func);
         }
 
+        /// <summary>
+        /// Setups async retry function with argument type IFlurlRequest
+        /// </summary>
+        /// <typeparam name="TResult">Type of Result of the function</typeparam>
+        /// <param name="argument">Base argument from which extension started</param>
+        /// <param name="func">Retry function</param>
+        /// <returns>Retry async retry context</returns>
         public static IFluentContextAsync<IFlurlRequest, TResult> WithRetryFuncAsync<TResult>(this IFlurlRequest argument,
             Func<IFlurlRequest, Func<IFlurlRequest, Task<TResult>>, Task<TResult>> func)
         {
